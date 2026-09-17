9b7941e1ca02075916177a9992765f460d5fc7fd
sg <dkglel1@gmaile.com>
2026-09-17 18:12:05 +0900

    Import I2C changes from dev.Kobokdong

--- MCAL/mcal_i2c.h
+++ MCAL/mcal_i2c.h
@@ -1,13 +1,31 @@
+#ifndef MCAL_I2C_H
+#define MCAL_I2C_H
+
+#include "common.h"
+
+typedef enum {
+    MCAL_I2C_OK = 0,  /* 정상적으로 통신 완료 */
+    MCAL_I2C_ERROR,   /* NACK 수신 또는 주소 불일치 등 오류 */
+    MCAL_I2C_BUSY,    /* 아직 통신 중 */
+    MCAL_I2C_TIMEOUT  /* 센서 응답 지연 */
+} mcal_i2c_status_t;
+
+bool mcal_i2c_init(uint8_t channel, uint8_t mode, uint32_t Freq);
+
+mcal_i2c_status_t mcal_i2c_write(uint8_t channel, uint16_t dev_addr,
+    uint16_t mem_addr, uint8_t mem_adddr_size, const uint8_t *data, uint16_t len, uint32_t timeout);
+
+mcal_i2c_status_t mcal_i2c_read(uint8_t channel, uint16_t dev_addr, uint16_t reg_addr,
+    uint8_t *data, uint16_t len, uint32_t timeout);
 
 
-
+#if 0
 //author: sgHyeon
 //date: 2026/08/24
 //version: 1
 
 
-#ifndef MCAL_I2C_H
-#define MCAL_I2C_H
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,5 +48,8 @@ mcal_i2c_status_t mcal_i2c_read(uint8_t i2c_instance, uint8_t dev_addr, uint8_t 
 #ifdef __cplusplus
 }
 #endif
+#endif
+
+
 
 #endif /* MCAL_I2C_H */
