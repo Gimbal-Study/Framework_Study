505e1f2b8ebf14974fe29ed3a04a11342ff4dea2
kobokdong <ggoji1109@gmail.com>
2026-09-21 21:12:06 +0900

    feat: option.h에서 이제 안쓰는 TIM4 관련 설정 제외

--- Board/NUCLEO_F411RE/option.h
+++ Board/NUCLEO_F411RE/option.h
@@ -16,20 +16,5 @@
 #define STACK_BASE	(RAM_END + 1)
 #define STACK_SIZE	(STACK_BASE - STACK_LIMIT)
 
-/*
- * 이 보드의 공통 시간원으로 TIM4 전체를 예약한다.
- * PWM, UART timeout 등 다른 기능에서 TIM4를 사용하지 않는다.
- *
- * 아래 항목들은 하나의 하드웨어 설정 묶음이다.
- */
-#define BOARD_TIMEBASE_TIMER_INSTANCE  4U
-#define BOARD_TIMEBASE_TIMER           TIM4
-#define BOARD_TIMEBASE_TIMER_IRQ       TIM4_IRQn
-#define BOARD_TIMEBASE_TIMER_HANDLER   TIM4_IRQHandler
-
-#define BOARD_TIMEBASE_CLOCK_EN_REG    (RCC->APB1ENR)
-#define BOARD_TIMEBASE_CLOCK_EN_MASK   RCC_APB1ENR_TIM4EN
-
-#define BOARD_TIMEBASE_TIMER_CLOCK_HZ  (TIMXCLK)
 
 #endif /* OPTION_H */
